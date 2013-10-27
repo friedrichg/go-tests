21fe2c04dff988888d0ffbc9dfcdec0dbbb0bc4b
Noname <noname@gmail.com>
2013-10-28 00:22:12 +0100

    save to index.html

--- wget/wget.go
+++ wget/wget.go
@@ -1,39 +1,43 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
-func usage(){
+func usage() {
 	fmt.Println("Usage: wget [OPTION]... [URL]...")
 	os.Exit(1)
 }
 
 func get(url string) {
-	l:=fmt.Println
-	resp, err:=http.Get(url)
+	l := fmt.Println
+	resp, err := http.Get(url)
 	if err != nil {
-		l("Error getting webpage",err)
+		l("Error getting webpage", err)
 		return
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		l("Error reading body")
+		l("Error reading body",err)
 		return
 	}
-	fmt.Println(string(body))
-	
+	err = ioutil.WriteFile("index.html", body,0644)
+	if err != nil {
+		l("Error writing to file",err)
+		return
+	}
+
 }
 
 func main() {
-	if (len(os.Args) < 2){
+	if len(os.Args) < 2 {
 		usage()
 	} else {
 		url := os.Args[1]
 		get(url)
 	}
-		
+
 }
